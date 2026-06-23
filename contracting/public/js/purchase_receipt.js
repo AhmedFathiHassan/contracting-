a6e0faea7bb42f6bf7b381f8db0b4a3a4154ccf7
AhmedFathiHassan <171283238+AhmedFathiHassan@users.noreply.github.com>
2026-06-23 16:03:46 +0300

    fix: show batch and serial in stock grids

--- contracting/public/js/purchase_receipt.js
+++ contracting/public/js/purchase_receipt.js
@@ -1,10 +1,15 @@
 function easyai_prepare_tracking_grid(frm) {
 	const grid = frm.fields_dict?.items?.grid;
 	if (!grid) return;
-	["batch_no", "serial_no"].forEach((fieldname) => {
-		grid.update_docfield_property(fieldname, "in_list_view", 1);
-		grid.update_docfield_property(fieldname, "hidden", 0);
+	const columns = { item_code: 2, qty: 1, batch_no: 2, serial_no: 2, rate: 1, amount: 2 };
+	(grid.docfields || []).forEach((field) => {
+		if (!field.fieldname) return;
+		grid.update_docfield_property(field.fieldname, "in_list_view", columns[field.fieldname] ? 1 : 0);
+		if (columns[field.fieldname]) grid.update_docfield_property(field.fieldname, "columns", columns[field.fieldname]);
 	});
+	["batch_no", "serial_no"].forEach((fieldname) => grid.update_docfield_property(fieldname, "hidden", 0));
+	grid.setup_visible_columns();
+	grid.refresh();
 }
 
 frappe.ui.form.on("Purchase Receipt", {
